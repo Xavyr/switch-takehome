e037f8228f134f1b3e717d149be5066f4b446d12
Xavyr Moss <xmoss10@gmail.com>
2024-10-16 20:46:24 -0600

    fix: typed apolloWrapper

--- switch/lib/ClientProviders.tsx
+++ switch/lib/ClientProviders.tsx
@@ -1,11 +1,16 @@
 "use client";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ReactNode } from "react";
 
 const client = new ApolloClient({
   uri: "/api/graphql",
   cache: new InMemoryCache(),
 });
 
-export default function ApolloWrapper({ children }) {
+interface ApolloWrapperProps {
+  children: ReactNode;
+}
+
+export default function ApolloWrapper({ children }: ApolloWrapperProps) {
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
